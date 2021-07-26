dcb2754ad162c26a07a31fb1ac0f6af75ed1d5d5
siro332 <lam332000@gmail.com>
2021-07-26 21:20:04 +0700

    Init

---
+++ src/main/java/com/utc/nckh/QLSVApplication.java
@@ -0,0 +1,13 @@
+package com.utc.nckh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QLSVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QLSVApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utc/nckh/QLSVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utc.nckh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QLSVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
